8c6976a6ccaee8f7e52e9c29aaf411330372b69b
Didac M <dida.m1989@gmail.com>
2017-06-02 12:32:41 +0200

    Basic ActiveAdmin + 3Users

---
+++ app/admin/movie.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Movie do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+	#Dani tutorial 02/06
+	permit_params :name, :stars, :director, :year
+
+end
