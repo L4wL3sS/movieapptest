abdd954ea1049c1e80318c59a6ef19d675cd4628
Didac M <dida.m1989@gmail.com>
2017-06-05 12:52:16 +0200

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,10 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     #super(resource)
-    settings_path
+    if authenticate_user {
+    	settings_path
+    }
+    
   end
 
 end
