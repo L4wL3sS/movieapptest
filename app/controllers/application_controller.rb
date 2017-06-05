90d7cc24e18fb0089c68e3584373756c9af1759a
Didac M <dida.m1989@gmail.com>
2017-06-05 13:04:31 +0200

    fix settings redirec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,9 +11,8 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     #super(resource)
-    if authenticate_user {
+    if :authenticate_user! 
     	settings_path
-    }
     end    
   end
 
