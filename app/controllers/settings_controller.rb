1d345d30d1fcb6d566136510b3101a1ab17a52d7
Didac M <dida.m1989@gmail.com>
2017-06-09 10:53:15 +0200

    settings functional deploy

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -3,40 +3,39 @@ class SettingsController < ApplicationController
 	layout "admin"
 
   def index
-  	#@setting = current_user.setting
-  	@setting = User.setting
+  	@setting = current_user.setting
   end
 
   def show
-  	#@setting = current_user.setting
-  	@setting = User.setting
+  	@setting = setting.find(params[:id])
   end	
 
-  def new
-  	#@setting = current_user.setting.new
-  	@setting = User.setting.new
+  def new  	
+  	@setting = current_user.build_setting
+  	#@setting = Setting.new #hack
   end
 
   def create
-  	@setting = current_user.setting.new(setting_params)
-  	if @setting.save
-  		redirect_to settings_path
-  	else
-  		render :new
-  	end
-   end
+	  @setting = current_user.build_setting(setting_params)
+	 
+	  if @setting.save
+			redirect_to settings_path
+	  else
+			render :new
+	  end
+  end
 
 	def edit 
-		@setting = current_user.setting.find(params[:id])
+		@setting = Setting.find(params[:id])
 	end
 
 	def update
-		@setting = current_user.setting.find(params[:id])
-  	if @setting.update_attributes(setting_params)
-  		redirect_to settings_path
-  	else
-  		render :index
-  	end
+		@setting = Setting.find(params[:id])
+		if @setting.update_attributes(setting_params)
+   		redirect_to settings_path
+	  else
+			render :index
+	  end
 	end
 
   def setting_params
