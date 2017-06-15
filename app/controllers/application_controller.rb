cc261f51f8ff30422d81d386d98a78b571c954dd
Didac M <dida.m1989@gmail.com>
2017-06-15 09:56:51 +0200

    improve users-adminuser log-logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,10 +12,10 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     #super(resource)
-    if user_signed_in? 
-    	settings_path
-    elsif admin_user_signed_in?
+    if admin_user_signed_in?
     	admin_root_path
+    elsif user_signed_in?     	
+      settings_path
     end    
   end
 
