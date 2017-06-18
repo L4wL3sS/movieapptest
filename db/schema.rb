a5883c4e1c688777ac6c0e066bf312b5be9f2d3b
Didac M <dida.m1989@gmail.com>
2017-06-18 10:54:28 +0200

    add price to movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615215427) do
+ActiveRecord::Schema.define(version: 20170618085223) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20170615215427) do
     t.datetime "image_updated_at"
     t.text     "url"
     t.string   "genre"
+    t.integer  "price"
   end
 
   create_table "payments", force: :cascade do |t|
