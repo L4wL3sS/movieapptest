98d79588153c85d0cc014d0a59a1844860653544
Didac M <dida.m1989@gmail.com>
2017-06-25 19:06:49 +0200

    uppsss

---
+++ app/mailers/user_notifier_mailer.rb
@@ -0,0 +1,17 @@
+class UserNotifierMailer < ApplicationMailer
+	default :from => 'lawl3ss.movie@gmail.com'
+  # send a signup email to the user, pass in the user object that   contains the user's email address
+  def send_signup_email(user)
+    @user = user
+    mail( :to => @user.email,
+    bcc: "mp.didac1989@gmail.com",
+    :subject => 'Thanks for signing up for our amazing app' )
+  end
+
+  def send_movie_payment(user)
+    @user = user
+    mail( :to => @user.email,
+    bcc: "mp.didac1989@gmail.com",
+    :subject => 'Thanks for buying in our amazing app' )
+  end
+end
